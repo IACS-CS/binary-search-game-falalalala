97392d1b29aaaf92e996c6a953ae2d10a499b90a
okearnan13 <126901221+okearnan13@users.noreply.github.com>
2024-09-12 13:01:30 +0000

    Save2

--- main.js
+++ main.js
@@ -3,8 +3,28 @@ import { TextInterface } from "text-interface";
 
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
-let ti = new TextInterface(app, "Billy's Fast Food Finder");
-ti.output("What is your name?");
-let name = await ti.readText();
-ti.output("Hello, " + name);
+var counter =  0;
+const resturants = ["Mcdonalds","Subway","Chick-fil-a","Dunkins","Pizza Hut","Chipotle","Tacobell","Panda Express"]
+let ti = new TextInterface(app, "Camy's Fast Food Finder");
+ti.output("Let Camy help you find a fast food resturant! Do want a sandwich of some sort? -Camy ");
+var answer= await ti.readYesOrNo();
+ti.clear();
+if(answer == true) {
+  counter += 4
+  ti.output("mmm yummy. Are you craving A burger or a chicken sandwich?");
+  answer = await ti.readYesOrNo();
+  if(answer == true){
+    counter += 2
+    ti.output("sounds good!")
+  }
+  if(answer == false){
+
+  }
+
+}
+if(answer == false) {
+
+}
+
+
 
