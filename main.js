d581531268c7b5582a25089098a36d52a6400d32
okearnan13 <126901221+okearnan13@users.noreply.github.com>
2024-09-13 13:46:32 +0000

    Save4

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ import { TextInterface } from "text-interface";
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
 var counter =  0;
-const resturants = ["Panda Express","Chipotle","Tacobell","Pizza Hut","Subway","Dunkins","Chick-fil-a","Mcdonald's"]
+const resturants = ["Panda Express","Pizza Hut","Tacobell","Chipotle","Subway","Dunkins","Chick-fil-a","Mcdonald's"]
 let ti = new TextInterface(app, "Camy's Fast Food Finder");
-ti.output("Let Camy help you find a fast food resturant! Do want a sandwich of some sort? -Camy ");
+ti.output("Let Camy help you find a fast food resturant! Do want a sandwich of some sort?");
 var answer= await ti.readYesOrNo();
 ti.clear();
 if(answer == true) {
@@ -30,23 +30,32 @@ if(answer == true) {
       counter += 1
     }
   }
-
+answer = true;
 }
 if(answer == false) {
   ti.output("Are you craving mexican food?");
   answer = await ti.readYesOrNo();
   ti.clear();
-  if(answer == true){
+  if(answer == true) {
     counter += 2
     ti.output("Interesting... do you want to customize your meal?")
+    answer = await ti.readYesOrNo();
     //asked chatgt for this question
+    if(answer == true){
+      counter += 1
+    }
+    answer = true;
   }
   if(answer == false){
-
+    ti.output("Hmmm how about some italian quisine?")
+    answer = await ti.readYesOrNo();
+    if(answer == true){
+      counter +=1
+    }
   }
 }
+ti.clear();
 let choice = resturants.at(counter);
 ti.output("Amazing, your resturant of choice is: " + choice)
-
 //billy has been an amazing peer partner for 9/12
 
