5376aa3ca2fbb8e2cba0ee686a58185d1c8fe2b8
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-09-19 05:48:05 -0400

    Update vite.config.js
    Use relative path for build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './', // Use relative paths
   build: {
     target: "esnext",
     minify: false     // Disables minification
